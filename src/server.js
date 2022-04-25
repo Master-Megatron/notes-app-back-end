98d3c5917bff79b78e630c29ee4d86eab85ff41a
Master-Megatron <102983156+Master-Megatron@users.noreply.github.com>
2022-04-26 00:31:43 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 /* eslint-disable no-undef */
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-const notes = require('./api/notes');
+// notes
 // const NotesService = require('./services/inMemory/NotesService');
+const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
-  notesService = new NotesService();
+  const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     // port: 5000,
     // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -20,13 +27,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
